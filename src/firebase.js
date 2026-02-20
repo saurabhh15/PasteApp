4aa8047b3e5844cf8ccc1097499a2232adda4516
Saurabh Kumar <saurabhsin141@gmail.com>
2026-02-21 01:09:14 +0530

    update firebase cloud storage

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyD3Q7WEnX0GsI8096u4ycVbIgPTg1D7_yE",
   authDomain: "pasteapp-6ac35.firebaseapp.com",
@@ -18,4 +13,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const db = getFirestore(app);
